f37459c4fd234131ff2509358a68b5c7b5e807fc
Stefan <stefan@verygood.ventures>
2024-07-25 10:04:33 -0400

    initial commit

---
+++ lib/src/test_amplify_in_dart.dart
@@ -0,0 +1,7 @@
+/// {@template test_amplify_in_dart}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class TestAmplifyInDart {
+  /// {@macro test_amplify_in_dart}
+  const TestAmplifyInDart();
+}
---
+++ test/src/test_amplify_in_dart_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:test/test.dart';
+import 'package:test_amplify_in_dart/test_amplify_in_dart.dart';
+
+void main() {
+  group('TestAmplifyInDart', () {
+    test('can be instantiated', () {
+      expect(TestAmplifyInDart(), isNotNull);
+    });
+  });
+}
